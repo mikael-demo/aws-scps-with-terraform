da04eff4405a33a3ba8f4e9a0b99a23b4c0bc2bf
Mikael Gulapa <mgulapa@veritionfund.com>
2024-01-05 18:41:44 +0800

    Initial checkin

--- modules/scp/main.tf
+++ modules/scp/main.tf
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 resource "aws_organizations_policy" "this" {
   name    = local.scp_name
   content = file(var.json_file)
--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 variable "region" {
   description = "region for deployment"
   type        = string
+  default     = "us-east-1"
 }
 
-variable "workload_ou" {
-  description = "list of workload OUs"
-  type        = list(string)
-}
+# variable "workload_ou" {
+#   description = "list of workload OUs"
+#   type        = list(string)
+# }
 
-variable "sandbox_ou" {
-  description = "list of sandbox OUs"
-  type        = list(string)
-}
+# variable "sandbox_ou" {
+#   description = "list of sandbox OUs"
+#   type        = list(string)
+# }
